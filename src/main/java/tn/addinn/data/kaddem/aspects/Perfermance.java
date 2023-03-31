7282db4999132ab914526bc335bbb0e2f7fcf723
baha <baha.rahmouni@esprit.tn>
2023-03-31 23:40:40 +0100

    commit AOP

---
+++ src/main/java/tn/addinn/data/kaddem/aspects/Perfermance.java
@@ -0,0 +1,23 @@
+package tn.addinn.data.kaddem.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@Aspect
+
+public class Perfermance {
+
+    @Around("execution(* tn.addinn.data.kaddem.services.*.*(..))")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object obj = pjp.proceed();
+        long elapsedTime = System.currentTimeMillis() - start;
+        log.info("Method execution time: " + elapsedTime + " milliseconds.");
+        return obj;
+    }
+}
